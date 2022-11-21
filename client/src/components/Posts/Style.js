d309d7c9def19f32306d60d86965699164c90c2c
Sandeepbangalore737 <sandeepbangalore737@gmail.com>
2022-11-21 10:02:48 +0530

    completed

---
+++ client/src/components/Posts/Style.js
@@ -0,0 +1,55 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles({
+  media: {
+    paddingTop: '56.25%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundBlendMode: 'darken',
+    height: 0,
+    backgroundImage: 'cover',
+   
+  },
+  border: {
+    border: 'solid',
+  },
+  fullHeightCard: {
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    borderRadius: '25px',
+    height: '100%',
+    position: 'relative',
+    padding: '4px',
+  },
+  overlay: {
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+    color: 'white',
+  },
+  overlay2: {
+    position: 'absolute',
+    top: '20px',
+    right: '20px',
+    color: 'white',
+  },
+  grid: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '20px',
+  },
+  title: {
+    padding: '0 16px',
+  },
+  cardActions: {
+    padding: '0 16px 8px 16px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
